8a8b0c768088f06a7aaa2ca9d2d7d80a56baa1ac
BartoszMiz <bartmiz.2004@gmail.com>
2021-05-30 00:48:37 +0200

    Add db migration on startup

--- TorBoard.Web/Startup.cs
+++ TorBoard.Web/Startup.cs
@@ -37,6 +37,12 @@ namespace TorBoard.Web
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+			{
+				var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+				context.Database.Migrate();
+			}
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
