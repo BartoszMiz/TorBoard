8f3b47f82a7e8852a0925cc5ea9cfa85c5d189a9
BartoszMiz <bartmiz.2004@gmail.com>
2021-06-03 13:23:53 +0200

    Add post form styling

--- TorBoard.Core/Models/Post.cs
+++ TorBoard.Core/Models/Post.cs
@@ -6,6 +6,9 @@ namespace TorBoard.Core.Models
 	public class Post : DbEntity
 	{
 		[Required] public DateTime CreationDate { get; set; }
-		[Required] [MinLength(10)] public string Content { get; set; }
+
+		[Required(ErrorMessage = "You have to write at least 10 characters.")]
+		[MinLength(10, ErrorMessage = "You have to write at least 10 characters.")]
+		public string Content { get; set; }
 	}
 }
