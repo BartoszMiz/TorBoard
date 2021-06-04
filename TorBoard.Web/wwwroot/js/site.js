0edfd0fac0f76c3d798cea8e162482915b8e332e
BartoszMiz <bartmiz.2004@gmail.com>
2021-06-04 17:27:09 +0200

    Add greentext formatting

--- TorBoard.Web/wwwroot/js/site.js
+++ TorBoard.Web/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const posts = document.getElementsByClassName("post");
+for (let i = 0; i < posts.length; i++)
+{
+	let post = posts[i];
+	post.innerHTML = post.innerHTML.replaceAll(/(&gt;.+)/g, "<span class=\"greentext\">$1</span>");
+}
+
